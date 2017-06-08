c5f900b0f2c78220359f8dca69868fa7d6dcbbf4
Samuel d'Huissier <samuel@aelia-informatique.fr>
2017-06-08 15:50:36 +0200

    Version initiale du projet

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name optibuseApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the optibuseApp
+ */
+angular.module('optibuseApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name optibuseApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the optibuseApp
+ */
+angular.module('optibuseApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
