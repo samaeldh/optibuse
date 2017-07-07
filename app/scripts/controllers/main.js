e0dc03fc07c2d8ba7ee5e54c916fbd7377e5104f
samaeldh <samaeldh@users.noreply.github.com>
2017-07-07 11:45:02 +0200

    Ajout jauge et divers modifs suite mail de Romain le 29-06

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,6 +9,49 @@
  */
 angular.module('optibuseApp')
   .controller('MainCtrl', function($scope, $http, OpticipApiConfig) {
+    var target = document.getElementById('jauge'); // your canvas element
+
+    var legend = document.getElementById('legendJauge');
+    var ctx = legend.getContext("2d");
+
+    // Rouge
+    ctx.beginPath();
+    ctx.fillStyle ="#f03e3e";
+    ctx.rect(10,10,30,16);
+    ctx.fill();
+
+    ctx.font = '12pt Calibri';
+    ctx.fillStyle = 'black';
+    ctx.fillText("Inadapté",50,24);
+
+    // Orange
+    ctx.beginPath();
+    ctx.fillStyle ="#ffa500";
+    ctx.rect(10,40,30,16);
+    ctx.fill();
+
+    ctx.font = '12pt Calibri';
+    ctx.fillStyle = 'black';
+    ctx.fillText("Vigilant",50,54);
+
+    // Rouge
+    ctx.beginPath();
+    ctx.fillStyle ="#30b32c";
+    ctx.rect(10,70,30,16);
+    ctx.fill();
+
+    ctx.font = '12pt Calibri';
+    ctx.fillStyle = 'black';
+    ctx.fillText("Bien",50,84);
+
+
+
+
+    var gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
+    gauge.maxValue = 60; // set max gauge value
+    gauge.setMinValue(0);  // Prefer setter over gauge.minValue = 0
+    gauge.animationSpeed = 32; // set animation speed (32 is default value)
+
     $scope.activeBtnRecherche = true;
     $scope.viewResultat = false;
     $scope.loadCriteres = function(search) {
@@ -17,6 +60,24 @@ angular.module('optibuseApp')
         .then(function(response) {
             $scope.criteres = response.data.criteres;
         });
+    };
+
+    $scope.afficheJauge = function() {
+        if (typeof $scope.data.jauge[0] === 'undefined') {
+            var valeur = 60;
+        } else {
+            if ( $scope.data.crit1 === '1' ) {
+                var valeur = ( $scope.data.jauge[0].note_globale / 42 ) * 60;
+            } else {
+                var valeur = ( $scope.data.jauge[0].note_globale / 39 ) * 60;
+            }
+        }
+        gauge.set(valeur); // set actual value
+    };
+
+    $scope.data = {
+        jauge:'',
+        crit1:''
     };
 
     $scope.loadCriteres();
@@ -29,7 +90,11 @@ angular.module('optibuseApp')
         if (Object.keys(tabCriteres).length == 6) {
             var url = OpticipApiConfig.searchUrl + '/recherche/';
             for (var i = 1; i <= 6; i++) {
-                    url += tabCriteres[i];
+                    if ( i === 1) { 
+                        $scope.data.crit1 = tabCriteres[i];
+                    }
+                    if (i === 6 && $scope.data.crit1 !== '1') url += '1';
+                    else url += tabCriteres[i];
                     if (i < 6 ) url += '/';
             }
             url += OpticipApiConfig.appid;
@@ -37,6 +102,8 @@ angular.module('optibuseApp')
             .then(function(response) {
                 $scope.activeBtnRecherche = false;
                 $scope.resultat = response.data.result;
+                $scope.data.jauge = response.data.result;
+                $scope.afficheJauge();
             }, function() {
                 $scope.activeBtnRecherche = true;
                 $scope.viewResultat = false;
@@ -50,9 +117,8 @@ angular.module('optibuseApp')
 });
 
 angular.module('optibuseApp')
-
-.filter('capitalize', function() {
-    return function(input) {
-      return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
-    }
+    .filter('capitalize', function() {
+        return function(input) {
+          return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
+        }
 });
